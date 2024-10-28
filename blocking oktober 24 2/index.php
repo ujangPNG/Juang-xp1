d3853bf14b0eeff654206961a3740b2082d8297c
Ujang <apasihsh@gmail.com>
2024-10-28 12:07:54 +0700

    modified:   blocking oktober 24 2/index.php

--- blocking oktober 24 2/index.php
+++ blocking oktober 24 2/index.php
@@ -45,4 +45,47 @@ if($op=='edit'){
         $error="data g ketemu";
     }
 }
-if (isset())
+if (isset($_POST['simpan'])){
+    $nim=$_POST['nim'];
+    $nama=$_POST['nama'];
+    $alamat=$_POST['alamat'];
+    $fakultas=$_POST['fakultas'];
+
+    if($nim&& $nama && $alamat && $fakultas){
+        if ($op='edit'){
+            $sql1="update mahasiswa set nim='$nim',nama='$nama',alamat='$alamat',fakultas='$fakultas' where id='$id'";
+            $q1=mysqli_query($koneksi,$sql1);
+            if($q1){
+                $sukses="data berhasil diupdate";
+            }else{
+                $error="bwop gagal";
+            }
+        }else{
+            $sql1="insert into mahasiswa(nim,nama,alamat,fakultas)values('$nim','$nama','$alamat','$fakultas')";
+            $q1=mysqli_query($koneksi,$sql1);
+            if($q1){
+                $sukses="data berhasil diupdate";
+            }else{
+                $error="bwop gagal";
+            }
+        }
+    }
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data mahasiswa</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <style>
+        .mx-auto{
+            width: 800px;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
